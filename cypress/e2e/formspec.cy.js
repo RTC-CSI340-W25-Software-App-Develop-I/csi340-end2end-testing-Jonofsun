d9097c9ff19e5030396946e07661589444c35d89
Tenshin <114845631+Jonofsun@users.noreply.github.com>
2025-02-27 21:25:07 -0800

    Test init
    env error

---
+++ cypress/e2e/formspec.cy.js
@@ -0,0 +1,21 @@
+var cy = require("cypress");
+
+describe("Submits a rating", () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000/");
+  });
+  it("Creates a booking", () => {
+    cy.get('[data-cy="input-name"]').type("Pizza House");
+    cy.get('[data-cy="input-address"]').type("123r PL NE, Seattle, USA");
+    cy.get('[data-cy="input-phone"]').type("111-123-1234");
+    cy.get('[data-cy="input-cuisine"]').type("Italian");
+    cy.get('[data-cy="input-rating"]').type("5");
+    cy.get('data-cy="rating-form"').submit();
+    cy.get(`[data-cy="Pizza House"] ul`).within(() => {
+      cy.contains("li", "123r PL NE, Seattle, USA").should("be.visible");
+      cy.contains("li", "111-123-1234").should("be.visible");
+      cy.contains("li", "Italian").should("be.visible");
+      cy.contains("li", "5").should("be.visible");
+    });
+  });
+});
